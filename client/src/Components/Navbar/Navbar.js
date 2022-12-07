235faa355daa1911834d0ad955d22c5e38ee0758
daniel <squaredfreezer@gmail.com>
2022-12-06 19:04:58 -0500

    optimized nav-bar switching between results page and tracks/artists page

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -1,27 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { logOut } from '../../Utils/Spotify'
-
+import './Navbar.css'
 
 const Navbar = () => {
 
-  const currentPage = window.location.pathname
-  if(currentPage === '/results') {
-    import('./NavbarResults.css')
-  } else if (currentPage === '/results/tracks') {
-    import('./NavbarTracks.css')
-  } else if (currentPage === '/results/artists') {
-    import('./NavbarArtists.css')
-  }
+  
+  useEffect(() => {
+    const currPage = window.location.pathname.split('/')[2]
+
+    if (currPage === 'tracks' || currPage === 'artists') {
+      document.getElementById(`nav-element-${currPage}`).style.color = 'whitesmoke'
+      import('./NavbarNoLogout.css')
+    }
+  }, [])
 
   return (
       <nav className = "navbar">   
         <div className = "navbar-links">
           <ul>
-            <a className = "nav-element-title" href='/results'>soundstats</a>
-            <a className = "nav-element-tracks" href='/results/tracks'>tracks</a>
-            <span className = "divider"> | </span>
-            <a className = "nav-element-artists" href='/results/artists'>artists</a>
-            <p className = "nav-element-logout" onClick = {logOut}>logout</p>
+            <a id = "nav-element-title" href='/results'>soundstats</a>
+            <a id = "nav-element-tracks" href='/results/tracks'>tracks</a>
+            <span id = "divider"> | </span>
+            <a id = "nav-element-artists" href='/results/artists'>artists</a>
+            <p id = "nav-element-logout" onClick = {logOut}>logout</p>
           </ul>
         </div>
       </nav>
