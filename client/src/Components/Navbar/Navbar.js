ba1eba7c09555eda96b3e31576fb6f6f9b7cc1d0
daniel <squaredfreezer@gmail.com>
2023-01-18 01:41:50 -0500

    add mobile navbar for tracks/genres/artists

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { getResultType, logOut } from '../../Utils/Spotify'
 import './Navbar.css'
 import { useNavigate } from 'react-router-dom'
@@ -6,41 +6,57 @@ import { useNavigate } from 'react-router-dom'
 const Navbar = () => {
 
   const navigate = useNavigate()
-  
-  const changeType = (range) => {
-    navigate(range)
+  const [resultType, setResultType] = useState(getResultType)
+  const changeType = (type) => {
+    navigate(`/results/${type}`)
+    setResultType(type)
   }
 
   useEffect(() => {
     const resultType = getResultType()
+    document.getElementById('nav-selector').addEventListener('change', function() {
+      changeType(this.value)
+    })
     if (resultType === 'tracks' || resultType === 'artists' || resultType === 'genres') {
       document.getElementById(`nav-element-${resultType}`).style.color = 'whitesmoke'
-      import('./NavbarNoLogout.css')
+      if (window.matchMedia("(max-width: 450px)").matches) {
+        document.getElementById('nav-element-logout').style.display = 'none'
+    }
+    } else {
+      document.getElementById('nav-selector').style.display = 'none'
     }
   }, [])
 
   return (
-    <nav className = 'navbar'>   
-      <div className = 'navbar-links'>
-        <ul>
-          <li id = 'nav-element-title' onClick={() => {changeType('/results')}}>
-            soundstats
-          </li>
-          <li id = 'nav-element-tracks' onClick={() => {changeType('/results/tracks')}}>
-            tracks
-          </li>
-          <span className = 'divider'> | </span>
-          <li id = 'nav-element-artists' onClick={() => {changeType('/results/artists')}}>
-            artists
-          </li>
-          <span className = 'divider'> | </span>
-          <li id = 'nav-element-genres' onClick={() => {changeType('/results/genres')}}>
-            genres
-          </li>
-          <p id = 'nav-element-logout' onClick = {logOut}>logout</p>
-        </ul>
-      </div>
-    </nav>
+    <>
+      <nav className = 'navbar'>   
+        <div className = 'navbar-links'>
+          <ul>
+            <li id = 'nav-element-title' onClick={() => {changeType('')}}>
+              soundstats
+            </li>
+
+            <select id= 'nav-selector' value={resultType}>
+              <option value='tracks'>tracks</option>
+              <option value='artists'>artists</option>
+              <option value='genres'>genres</option>
+            </select>
+            <li id = 'nav-element-tracks' onClick={() => {changeType('tracks')}}>
+              tracks
+            </li>
+            <span className = 'divider'> | </span>
+            <li id = 'nav-element-artists' onClick={() => {changeType('artists')}}>
+              artists
+            </li>
+            <span className = 'divider'> | </span>
+            <li id = 'nav-element-genres' onClick={() => {changeType('genres')}}>
+              genres
+            </li>
+            <p id = 'nav-element-logout' onClick = {logOut}>log-out</p>
+          </ul>
+        </div>
+      </nav>
+    </>
   )
 }
 export default Navbar
